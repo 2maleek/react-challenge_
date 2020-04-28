fa87ac7027c1f282e3b48ae717e863ea4f6432df
2maleek <222malikibrahim@gmail.com>
2020-04-28 15:33:59 +0700

    using custom hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import CardMovies from './components/CardMovies';
 import { Button, Spinner } from 'react-bootstrap';
+import useFetch from './hooks/useFetch';
 
 function App() {
-  const [listMovies, setListMovies] = useState(['pertmax'])
 
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=9435ef832f577bb7037f8360b55808ba&language=en-ID&page=1')
-    .then(response => response.json())
-    .then(data => {
-      if(data.status_code) {
-        throw data
-      }
-      setListMovies(data.results)
-    })
-    .catch(error => {
-      console.log(error.status_message)
-    })
-  }, [])
+  const [listMovies, loading, error] = useFetch('https://api.themoviedb.org/3/movie/popular?api_key=9435ef832f577bb7037f8360b55808ba&language=en-ID&page=1')
 
   return (
     <>
       <h2>Popular Movie</h2>
-      <Button variant="primary" disabled className="text-center">
-        <Spinner
-          as="span"
-          animation="grow"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-        Loading...
-      </Button>
+      {loading ?
+        <Button variant="primary" disabled className="text-center">
+          <Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          Loading...
+        </Button>
+       : ''}
       <CardMovies listMovies={listMovies}/>
     </>
   );
