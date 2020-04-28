6bcb3b72ef6ab91ba5408ae6d2b6c5169bacffd8
2maleek <222malikibrahim@gmail.com>
2020-04-28 10:47:50 +0700

    add react-bootstrap use state, props, and lifecycle

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import CardMovies from './components/CardMovies';
+import { Button, Spinner } from 'react-bootstrap';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      listMovies: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=9435ef832f577bb7037f8360b55808ba&language=en-ID&page=1')
+    .then(res => res.json())
+    .then(resJson => {
+      this.setState({
+        listMovies: resJson.results
+      })
+      console.log(this.state.listMovies)
+    })
+  }
   render() {
     return (
       <>
-        <h1>Hahahahaha</h1>
-        <CardMovies />
+        <h2>Popular Movie</h2>
+        <Button variant="primary" disabled className="text-center">
+          <Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          Loading...
+        </Button>
+        <CardMovies listMovies={this.state.listMovies}/>
       </>
     )
   }
--- client/src/components/CardMovies.js
+++ client/src/components/CardMovies.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
 
 class CardMovies extends Component {
-  constructor() {
-    super()
-    this.state = {
-      listMovies: []
-    }
-  }
-  componentDidMount() {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=9435ef832f577bb7037f8360b55808ba&language=en-ID&page=1')
-    .then(res => res.json())
-    .then(resJson => {
-      this.setState({
-        listMovies: resJson.results
-      })
-      console.log(this.state.listMovies)
-    })
-  }
+
   render() {
     return (
-      <>
-        {this.state.listMovies.map((movie, idx) => {
-          return (
-            <Card style={{ width: '18rem' }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          )
-        })}
-      </>
+      <div className="container">
+        <div className="row">
+          {this.props.listMovies.map((movie, idx) => {
+            let imageUrl = "http://image.tmdb.org/t/p/w154"
+            imageUrl += movie.poster_path
+            return (
+              <div className="cols-12">
+                <Card key={ idx } style={{ width: '12rem' }}>
+                  <Card.Img variant="top" src={imageUrl} />
+                  <Card.Body>
+                    <Card.Title>{ movie.title }</Card.Title>
+                    <Card.Text>
+                      { movie.release_date }
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     )
   }
 }
